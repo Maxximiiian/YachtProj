206c23fdd468f0a897de723e14c2ee989b368781
Maxximiiian <SantoCastor@yandex.ru>
2022-09-08 11:51:42 +0300

    init proj 3

--- client/src/components/main/Main.jsx
+++ client/src/components/main/Main.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import NotAuth from '../auth/NotAuth';
 
 export default function Main() {
   const { auth } = useSelector((state) => state);
